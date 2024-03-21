f0fd3d19884ee18bb0979dad5699da3842310b89
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2024-03-21 22:32:56 +0700

    initial commit

---
+++ src/test/java/muhammadrezkiananda/springdesignpatternscreational/SpringDesignPatternsCreationalApplicationTests.java
@@ -0,0 +1,13 @@
+package muhammadrezkiananda.springdesignpatternscreational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternsCreationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
